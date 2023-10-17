966dacf463c6517b32afab1dbb56591da67dee9a
Kohei Dunnet <kdunnet1@my.bcit.ca>
2023-10-17 11:35:07 -0700

    First commit after Demo07 created loginmenu

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -7,3 +7,36 @@ function loadSkeleton(){
     console.log($('#footerPlaceholder').load('./text/footer.html'));
 }
 loadSkeleton();  //invoke the function
+
+//---------------------------------------------------
+// This function loads the parts of your skeleton 
+// (navbar, footer, and other things) into html doc. 
+//---------------------------------------------------
+function loadSkeleton() {
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {                   //if the pointer to "user" object is not null, then someone is logged in
+            // User is signed in.
+            // Do something for the user here.
+            console.log($('#navbarPlaceholder').load('./text/nav_after_login.html'));
+            console.log($('#footerPlaceholder').load('./text/footer.html'));
+        } else {
+            // No user is signed in.
+            console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
+            console.log($('#footerPlaceholder').load('./text/footer.html'));
+        }
+    });
+}
+loadSkeleton(); //invoke the function
+
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+      }).catch((error) => {
+        // An error happened.
+      });
+}
